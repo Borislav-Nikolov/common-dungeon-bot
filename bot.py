976691beead5673b934f767055ea64f70d5a4114
Borislav <borislav.nikolov@pcloud.com>
2023-02-03 19:24:30 +0200

    Add generating magic shop string

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import discord
-import responses
 import os
 import magicshop
 from dotenv import load_dotenv
@@ -13,16 +12,8 @@ channels_to_messages = dict()
 # use firebase to store data?
 
 
-async def send_message(message, user_message, is_private):
-    try:
-        response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
-
-
 def run_discord_bot():
-    bot_token = str(os.getenv('token'))
+    bot_token = str(os.getenv('TOKEN'))
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
@@ -47,19 +38,7 @@ def run_discord_bot():
 
         print(f'{username} said: "{user_message}" ({channel})')
 
-        if user_message[0] == '$':
-            response = magicshop.get_item_names()
-            await message.channel.send(response)
-        elif user_message[0] == '@':
-            await message.add_reaction('🪙')
-        elif user_message[0] == '!':
-            user_message = user_message[1:]
-            last_bot_message = await message.channel.fetch_message(channels_to_messages[message.channel.id])
-            await last_bot_message.edit(content=user_message)
-        elif user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
+        if message.channel.name == '💰-shop' and user_message == '$generate':
+            await message.channel.send(magicshop.get_item_names())
 
     client.run(bot_token)
--- magicshop.py
+++ magicshop.py
@@ -8,48 +8,77 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1nnB8VmIUtkYCIQXcaQIsEHj10K7OkELYRloSJmbK-Ow'
-SAMPLE_RANGE_NAME = 'Magic Shop!A2:E'
+randomized_item_list_spreadsheet = '1nnB8VmIUtkYCIQXcaQIsEHj10K7OkELYRloSJmbK-Ow'
+randomized_item_list_spreadsheet_range = 'Magic Shop!A2:E'
 
 
 def get_item_names() -> str:
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
     try:
         service = build('sheets', 'v4', credentials=creds)
 
-        # Call the Sheets API
         sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME).execute()
+        result = sheet.values().get(spreadsheetId=randomized_item_list_spreadsheet,
+                                    range=randomized_item_list_spreadsheet_range).execute()
         values = result.get('values', [])
 
         if not values:
             print('No data found.')
             return ''
 
+        values.pop(0)
         item_names = ""
         for row in values:
-            item_names += row[1] + '\n'
+            print(row)
+            token_cost = '1 common token' if len(row) < 3 else tokens_per_rarity(row[2], row[4])
+            name = str(f'**{row[1]}** - {token_cost}\n')
+            item_names += name
 
         return item_names
     except HttpError as err:
         print(err)
+
+
+COMMON = "common"
+UNCOMMON = "uncommon"
+RARE = "rare"
+VERY_RARE = "very rare"
+LEGENDARY = "legendary"
+TYPE_MINOR = "minor"
+TYPE_MAJOR = "major"
+
+
+def tokens_per_rarity(rarity, rarity_type) -> str:
+    rarity = rarity.lower()
+    rarity_type = rarity_type.lower()
+    if rarity == COMMON:
+        '1 common token'
+    elif rarity == UNCOMMON and rarity_type == TYPE_MINOR:
+        '3 uncommon tokens'
+    elif rarity == UNCOMMON and rarity_type == TYPE_MAJOR:
+        '6 uncommon tokens'
+    elif rarity == RARE and rarity_type == TYPE_MINOR:
+        '4 rare tokens'
+    elif rarity == RARE and rarity_type == TYPE_MAJOR:
+        '8 rare tokens'
+    elif rarity == VERY_RARE and rarity_type == TYPE_MINOR:
+        '5 very rare tokens'
+    elif rarity == VERY_RARE and rarity_type == TYPE_MAJOR:
+        '10 very rare tokens'
+    elif rarity == LEGENDARY and rarity_type == TYPE_MINOR:
+        '5 legendary tokens'
+    elif rarity == LEGENDARY and rarity_type == TYPE_MAJOR:
+        '10 legendary tokens'
