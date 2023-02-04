f40029c51e62396a86a5f1140881b800f9ec6439
Borislav <borislav.nikolov@pcloud.com>
2023-02-04 20:44:29 +0200

    Add firebase database Add item selling

--- bot.py
+++ bot.py
@@ -31,17 +31,13 @@ def run_discord_bot():
 
         print(f'{username} said: "{user_message}" ({channel})')
 
-        if message.channel.name == '💰-shop' and user_message == '$generate':
-            await message.channel.send(magicshop.generate_new_magic_shop())
-        if message.channel.name == '💰-shop' and len(user_message) >= 3 and user_message[0] == '$' and user_message[1:3].isnumeric():
-            shop_message = await message.channel.fetch_message(firebase.shop_message_ref.get()["message_id"])
-            shop_string = magicshop.sell_item(int(user_message[1:3]))
-            await shop_message.edit(content=shop_string)
-            await message.add_reaction('🪙')
-        elif user_message.lower().startswith('bogdy'):
-            if 'give me love' in user_message.lower():
-                await message.add_reaction('❤')
-            else:
-                await message.channel.send('Sorry, I\'m quite busy at the moment.')
+        if message.channel.name == '💰-shop' or message.channel.name == 'spam':
+            if user_message == '$generate':
+                await message.channel.send(magicshop.generate_new_magic_shop())
+            elif user_message[0] == '$' and len(user_message) >= 2 and user_message[1:3].isnumeric():
+                shop_message = await message.channel.fetch_message(firebase.shop_message_ref.get()["message_id"])
+                shop_string = magicshop.sell_item(int(user_message[1:3]))
+                await shop_message.edit(content=shop_string)
+                await message.add_reaction('🪙')
 
     client.run(bot_token)
