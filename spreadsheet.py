a80e01bb267472cefe31a3413c37e76401aa0253
Borislav <borislav.nikolov@pcloud.com>
2023-03-27 12:03:07 +0300

    WIP: Add more items

--- spreadsheet.py
+++ spreadsheet.py
@@ -534,15 +534,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Perfume of Bewitching",
+            price=price_25,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="This tiny vial contains magic perfume, enough for one use. You can use an action to apply the perfume to yourself, and its effect lasts 1 hour. For the duration, you have advantage on all Charisma checks directed at humanoids of challenge rating 1 or lower. Those subjected to the perfume's effect are not aware that they've been influenced by magic.",
+            last_item=False
         )
     )
     item_id += 1
@@ -550,15 +550,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Pipe of Smoke Monsters",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="While smoking this pipe, you can use an action to exhale a puff of smoke that takes the form of a single creature, such as a dragon, a flumph, or a froghemoth. The form must be small enough to fit in a 1-foot cube and loses its shape after a few seconds, becoming an ordinary puff of smoke.",
+            last_item=False
         )
     )
     item_id += 1
@@ -566,15 +566,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Pole of Angling",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="While holding this 10-foot pole, you can speak a command word and transform it into a fishing pole with a hook, a line, and a reel. Speaking the command word again changes the fishing pole back into a normal 10-foot pole.",
+            last_item=False
         )
     )
     item_id += 1
@@ -582,15 +582,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Pole of Collapsing",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="While holding this 10-foot pole, you can use an action to speak a command word and cause it to collapse into a 1-foot-long rod, for ease of storage. The pole's weight doesn't change. You can use an action to speak a different command word and cause the rod to revert to a pole; however, the rod will elongate only as far as the surrounding space allows.",
+            last_item=False
         )
     )
     item_id += 1
@@ -598,15 +598,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Pot of Awakening",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="If you plant an ordinary shrub in this 10-pound clay pot and let it grow for 30 days, the shrub magically transforms into an awakened shrub (see the Monster Manual for statistics) at the end of that time. When the shrub awakens, its roots break the pot, destroying it.\\n\\nThe awakened shrub is friendly toward you. Absent commands from you, it does nothing.",
+            last_item=False
         )
     )
     item_id += 1
@@ -614,15 +614,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Rope of Mending",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="You can cut this 50-foot coil of hempen rope into any number of smaller pieces, and then use an action to speak a command word and cause the pieces to knit back together. The pieces must be in contact with each other and not otherwise in use. A rope of mending is forever shortened if a section of it is lost or destroyed.",
+            last_item=False
         )
     )
     item_id += 1
@@ -630,15 +630,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Ruby of the War Mage",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=True,
+            official=True,
+            banned=False,
+            description="Etched with eldritch runes, this 1-inch-diameter ruby allows you to use a simple or martial weapon as a spellcasting focus for your spells. For this property to work, you must attach the ruby to the weapon by pressing the ruby against it for at least 10 minutes. Thereafter, the ruby can't be removed unless you detach it as an action or the weapon is destroyed. Not even an Antimagic Field causes it to fall off. The ruby does fall off the weapon if your attunement to the ruby ends.",
+            last_item=False
         )
     )
     item_id += 1
@@ -646,15 +646,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Shield of Expression",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="The front of this shield is shaped in the likeness of a face. While bearing the shield, you can use a bonus action to alter the face's expression.",
+            last_item=False
         )
     )
     item_id += 1
@@ -662,15 +662,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Smoldering Armor",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="Wisps of harmless, odorless smoke rise from this armor while it is worn.",
+            last_item=False
         )
     )
     item_id += 1
@@ -678,15 +678,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Staff of Adornment",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="If you place an object weighing no more than 1 pound (such as a shard of crystal, an egg, or a stone) above the tip of the staff while holding it, the object floats an inch from the staff's tip and remains there until it is removed or until the staff is no longer in your possession. The staff can have up to three such objects floating over its tip at any given time. While holding the staff, you can make one or more of the objects slowly spin or turn in place.",
+            last_item=False
         )
     )
     item_id += 1
@@ -694,15 +694,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Staff of Birdcalls",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="This wooden staff is decorated with bird carvings. It has 10 charges. While holding it, you can use an action to expend 1 charge from the staff and cause it to create one of the following sounds out to a range of 60 feet: a finch's chirp, a raven's caw, a duck's quack, a chicken's cluck, a goose's honk, a loon's call, a turkey's gobble, a seagull's cry, an owl's hoot, or an eagle's shriek.\\n\\nThe staff regains 1d6 + 4 expended charges daily at dawn. If you expend the last charge, roll a d20. On a 1, the staff explodes in a harmless cloud of bird feathers and is lost forever.",
+            last_item=False
         )
     )
     item_id += 1
@@ -710,15 +710,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Staff of Flowers",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="This wooden staff has 10 charges. While holding it, you can use an action to expend 1 charge from the staff and cause a flower to sprout from a patch of earth or soil within 5 feet of you, or from the staff itself. Unless you choose a specific kind of flower, the staff creates a mild-scented daisy. The flower is harmless and nonmagical, and it grows or withers as a normal flower would.\\n\\nThe staff regains 1d6 + 4 expended charges daily at dawn. If you expend the last charge, roll a d20. On a 1, the staff turns into flower petals and is lost forever.",
+            last_item=False
         )
     )
     item_id += 1
@@ -726,15 +726,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Talking Doll",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=True,
+            official=True,
+            banned=False,
+            description="While this stuffed doll is within 5 feet of you, you can spend a short rest telling it to say up to six phrases, none of which can be more than six words long, and set a condition under which the doll speaks each phrase. You can also replace old phrases with new ones. Whatever the condition, it must occur within 5 feet of the doll to make it speak. For example, whenever someone picks up the doll, it might say, 'I want a piece of candy.' The doll's phrases are lost when your attunement to the doll ends.",
+            last_item=False
         )
     )
     item_id += 1
@@ -742,15 +742,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Tankard of Sobriety",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="This tankard has a stern face sculpted into one side. You can drink ale, wine, or any other nonmagical alcoholic beverage poured into it without becoming inebriated. The tankard has no effect on magical liquids or harmful substances such as poison.",
+            last_item=False
         )
     )
     item_id += 1
@@ -758,15 +758,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Unbreakable Arrow",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="This arrow can't be broken, except when it is within an Antimagic Field.",
+            last_item=False
         )
     )
     item_id += 1
@@ -774,15 +774,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Veteran's Cane",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="When you grasp this walking cane and use a bonus action to speak the command word, it transforms into an ordinary longsword and ceases to be magical.",
+            last_item=False
         )
     )
     item_id += 1
@@ -790,15 +790,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Walloping Ammunition",
+            price=price_25,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="This ammunition packs a wallop. A creature hit by the ammunition must succeed on a DC 10 Strength saving throw or be knocked prone.",
+            last_item=False
         )
     )
     item_id += 1
@@ -806,15 +806,15 @@ def add_items_not_in_spreadsheet(items_file_write, item_id: int):
     items_file_write.write(
         create_json_item_string(
             item_id=item_id,
-            name="",
-            price="",
-            rarity="",
-            rarity_level="",
-            attunement=,
-            official=,
-            banned=,
-            description="",
-            last_item=
+            name="Wand of Conducting",
+            price=price_50,
+            rarity=common_str,
+            rarity_level=minor_str,
+            attunement=False,
+            official=True,
+            banned=False,
+            description="This wand has 3 charges. While holding it, you can use an action to expend 1 of its charges and create orchestral music by waving it around. The music can be heard out to a range of 60 feet and ends when you stop waving the wand.\\n\\nThe wand regains all expended charges daily at dawn. If you expend the wand's last charge, roll a d20. On a 1, a sad tuba sound plays as the wand crumbles to dust and is destroyed.",
+            last_item=False
         )
     )
     item_id += 1
