29727b567d577c89cd969da539c8b830d2154538
Borislav <borislav.nikolov@pcloud.com>
2024-10-13 10:55:23 +0300

    Mix items1 and items 2

--- source/itemssource.py
+++ source/itemssource.py
@@ -1,14 +1,16 @@
 from firebase_admin import db
 from source.sourcefields import *
 import json
+import copy
 
+global items_ref
 global items_ref_2
 
 
 def init_items_source(is_test: bool):
     prefix = "/test" if is_test else ""
     global items_ref_2
-    items_ref_2 = db.reference(f"{prefix}/all_items2")
+    items_ref_2 = db.reference(f"all_items2")
 
 
 def get_all_items() -> list:
@@ -34,29 +36,64 @@ def get_all_major_items():
 
 
 # All of bellow's script is used to initialize the items in the database.
+def mix_all_items_and_all_items_2():
+    items: list = items_ref.get()
+    items2: list = items_ref_2.get()
+    new_list: list = copy.deepcopy(items2)
+    for item in items:
+        rarity_level = item[field_rarity_level]
+        if rarity_level == 'MINOR':
+            item[field_rarity_level] = init_TYPE_MINOR
+        elif rarity_level == 'MAJOR':
+            item[field_rarity_level] = init_TYPE_MAJOR
+        has_match = False
+        for item2 in items2:
+            name1 = item[field_name]
+            name2 = item2[field_name]
+            if name1 == name2 or name1 in name2 or name2 in name1 or name1[:7] in name2 or name2[:7] in name1:
+                has_match = True
+        if not has_match:
+            new_list.append(item)
+    items_ref_2.set(new_list)
+
+
 def init_in_firebase():
     new_items = list()
     with open('items_update.json', 'r', encoding='utf-8') as items:
-        items_list = json.load(items)['item']
+        items_json = json.load(items)
+        items_list = items_json['item']
+        grouped_items_list = items_json['itemGroup']
     for item in items_list:
-        if field_name in item and len(item[field_name]) > 0 and field_source in item and item[field_source] in permitted_sources and field_rarity in item and item[field_rarity] in permitted_rarities:
-            translation_table = str.maketrans('', '', '$#[]/.')
-            item_name = item['name'].translate(translation_table)
-            new_items.append({
-                "name": item_name,
-                "attunement": True if field_req_attune in item and ((isinstance(item[field_req_attune], bool) and item[
-                    field_req_attune]) or (isinstance(item[field_req_attune], str) and len(item[field_req_attune]) > 0))
-                else False,
-                "banned": False,
-                "official": True,
-                "price": "undetermined",
-                "rarity": item[field_rarity],
-                "rarity_level": item[field_tier] if field_tier in item and item[field_tier] in permitted_rarity_levels
-                else init_TYPE_MAJOR,
-                "description": description_from_entries(item[field_entries]) if field_entries in item
-                else "missing description"
-            })
+        append_new_item(new_items, item)
+    for item in grouped_items_list:
+        if field_items in item and len(item[field_items]) > 0:
+            for specific_name in item[field_items]:
+                fixed_name = specific_name.split("|")[0]
+                item_copy = copy.deepcopy(item)
+                item_copy[field_name] = fixed_name
+                append_new_item(new_items, item_copy)
     items_ref_2.set(new_items)
+
+
+def append_new_item(new_items: list, item):
+    if field_name in item and len(item[field_name]) > 0 and field_source in item and item[
+            field_source] in permitted_sources and field_rarity in item and item[field_rarity] in permitted_rarities:
+        translation_table = str.maketrans('', '', '$#[]/.')
+        item_name = item['name'].translate(translation_table)
+        new_items.append({
+            "name": item_name,
+            "attunement": True if field_req_attune in item and ((isinstance(item[field_req_attune], bool) and item[
+                field_req_attune]) or (isinstance(item[field_req_attune], str) and len(item[field_req_attune]) > 0))
+            else False,
+            "banned": False,
+            "official": True,
+            "price": "undetermined",
+            "rarity": item[field_rarity],
+            field_rarity_level: item[field_tier] if field_tier in item and item[field_tier] in permitted_rarity_levels
+            else init_TYPE_MAJOR,
+            "description": description_from_entries(item[field_entries]) if field_entries in item
+            else "missing description"
+        })
 
 
 def description_from_entries(entries: list[str, dict]) -> str:
@@ -81,6 +118,7 @@ field_tier = 'tier'
 field_entries = 'entries'
 field_type = 'type'
 field_items = 'items'
+field_rarity_level = "rarity_level"
 
 permitted_sources = ['PHB', 'DMG', 'MM', 'VGM', 'XGE', 'MTF', 'TCE', 'FTD', 'MPMM', 'BGG', 'BMT',
                      'SCAG', 'GGR', 'AI', 'ERLW', 'EGW', 'MOT', 'VRGR', 'SCC', 'AAG', 'SatO',
