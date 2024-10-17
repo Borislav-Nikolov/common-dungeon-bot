a9dc6dcd841250552be5c94d7b3246f98922d85f
Borislav <borislav.nikolov@pcloud.com>
2024-10-17 19:11:21 +0300

    implement backend communication for the magic shop

--- main.py
+++ main.py
@@ -66,20 +66,18 @@ def run_all_test_calls():
 load_dotenv()
 
 if __name__ == '__main__':
-    run_all_test_calls()
-    # bot_token = str(os.getenv('TOKEN'))
-    # is_test = bot_token == str(os.getenv('TEST_TOKEN'))
-    # project_url = str(os.getenv('FIREBASE_PROJECT'))
-    # # initialize firebase realtime database components
-    # firebase.init_firebase(project_url)
-    # itemssource.init_items_source(is_test)
-    # magicshopsource.init_shop_source(is_test)
-    # channelssource.init_channels_source(is_test)
-    # playerssource.init_players_source(is_test)
-    # staticshopsource.init_static_shop_source(is_test)
-    # postssource.init_posts_source(is_test)
-    # consolesource.init_console_source(is_test)
-    # rolepermissionssourse.init_role_permissions_source(is_test)
-    # # start bot
-    # bot.run_discord_bot(bot_token=bot_token, allowed_guild_id=str(os.getenv('ALLOWED_GUILD_ID')))
+    bot_token = str(os.getenv('TEST_TOKEN'))
+    is_test = bot_token == str(os.getenv('TEST_TOKEN'))
+    project_url = str(os.getenv('FIREBASE_PROJECT'))
+    # initialize firebase realtime database components
+    firebase.init_firebase(project_url)
+    itemssource.init_items_source(is_test)
+    channelssource.init_channels_source(is_test)
+    playerssource.init_players_source(is_test)
+    staticshopsource.init_static_shop_source(is_test)
+    postssource.init_posts_source(is_test)
+    consolesource.init_console_source(is_test)
+    rolepermissionssourse.init_role_permissions_source(is_test)
+    # start bot
+    bot.run_discord_bot(bot_token=bot_token, allowed_guild_id=str(os.getenv('TEST_ALLOWED_GUILD_ID')))
 
--- source/magicshopsource.py
+++ source/magicshopsource.py
@@ -1,18 +1,13 @@
-from firebase_admin import db
-
-
-global shop_ref
-
-
-def init_shop_source(is_test: bool):
-    prefix = "/test" if is_test else ""
-    global shop_ref
-    shop_ref = db.reference(f"{prefix}/magic_shop_items")
+import requests
 
 
 def get_magic_shop_items() -> list:
-    return shop_ref.get()
+    return requests.get('http://localhost:8081/magic_shop_items').json()
 
 
 def set_in_magic_shop(items: list):
-    shop_ref.set(items)
+    response = requests.post('http://localhost:8081/set_magic_shop_items', json=items)
+    if response.status_code == 200:
+        print("Set magic shop request was successful.")
+    else:
+        print(f"Set magic shop request failed with status code: {response.status_code}.")
