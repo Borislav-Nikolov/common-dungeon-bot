8b0bd8bd50a0f10954aa19961beb2dd561c5602f
Borislav <borislav.nikolov@pcloud.com>
2024-12-21 10:02:26 +0200

    Return test code

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == '__main__':
-    bot_token = str(os.getenv('TOKEN'))
+    bot_token = str(os.getenv('TEST_TOKEN'))
     is_test = bot_token == str(os.getenv('TEST_TOKEN'))
     project_url = str(os.getenv('FIREBASE_PROJECT'))
     # initialize firebase realtime database components
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     postssource.init_posts_source(is_test)
     consolesource.init_console_source(is_test)
     rolepermissionssourse.init_role_permissions_source(is_test)
-    api.base.init_api(test=is_test, local=False)
+    api.base.init_api(test=is_test, local=True)
     # start bot
-    bot.run_discord_bot(bot_token=bot_token, allowed_guild_id=str(os.getenv('ALLOWED_GUILD_ID')))
+    bot.run_discord_bot(bot_token=bot_token, allowed_guild_id=str(os.getenv('TEST_ALLOWED_GUILD_ID')))
 
