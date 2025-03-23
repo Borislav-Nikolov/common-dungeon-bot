ec7907f6c6e5c559833ada5eabb0ca749f02df8c
Borislav <borislav.nikolov@pcloud.com>
2025-03-23 10:21:37 +0200

    Remove firebase from project

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import api.base
 import bot
-import firebase
 import os
-from source import staticshopsource, rolepermissionssourse
 
 from dotenv import load_dotenv
 
@@ -13,8 +11,6 @@ if __name__ == '__main__':
     bot_token = str(os.getenv('TEST_TOKEN'))
     is_test = bot_token == str(os.getenv('TEST_TOKEN'))
     project_url = str(os.getenv('FIREBASE_PROJECT'))
-    # initialize firebase realtime database components
-    firebase.init_firebase(project_url)
     api.base.init_api(test=is_test, local=True)
     # start bot
     bot.run_discord_bot(bot_token=bot_token, allowed_guild_id=str(os.getenv('TEST_ALLOWED_GUILD_ID')))
