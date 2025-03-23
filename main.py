9130f55e5264e4eb77f4d78407c1c6472227b984
Borislav <borislav.nikolov@pcloud.com>
2025-03-23 10:19:03 +0200

    Delegate staticshopsource firebase calls to backend

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
     project_url = str(os.getenv('FIREBASE_PROJECT'))
     # initialize firebase realtime database components
     firebase.init_firebase(project_url)
-    staticshopsource.init_static_shop_source(is_test)
     api.base.init_api(test=is_test, local=True)
     # start bot
     bot.run_discord_bot(bot_token=bot_token, allowed_guild_id=str(os.getenv('TEST_ALLOWED_GUILD_ID')))
